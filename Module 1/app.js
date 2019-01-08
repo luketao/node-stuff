8b15925d9310c7c015b4c3f307d973d0147afc5f
Luke Tao <coolman0893@gmail.com>
2019-01-07 21:46:35 -0800

    Onto Module 2

--- Module 1/app.js
+++ Module 1/app.js
@@ -1,8 +1,11 @@
 const http = require('http');
 
-const routes = require('./routes');
+const express = require('express');
 
-console.log(routes.someText);
-const server = http.createServer(routes.handler);
+const app = express();
+
+app.use((req, res, next) => {});
+
+const server = http.createServer(app);
 
 server.listen(3000);
